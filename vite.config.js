f8e3045bc1f5002ca07bd84e1aaa81fedd238e50
Maikpwwq <mariasf@correo.udistrital.edu.co>
2023-08-11 13:38:58 -0500

    feat: @mui/material/Button

---
+++ vite.config.js
@@ -0,0 +1,36 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import ssr from "vite-plugin-ssr/plugin";
+import { cjsInterop } from "vite-plugin-cjs-interop";
+import vercel from "vite-plugin-vercel";
+import vercelSsr from "@magne4000/vite-plugin-vercel-ssr";
+
+export default defineConfig(async ({ command, mode }) => {
+  console.log("defineConfig", command, mode);
+  return {
+    plugins: [
+      react(),
+      ssr({
+        // Use the default pre-render config:
+        prerender: true,
+        ...(process.env.NODE_ENV === "production"
+          ? {
+              noExternal: [
+                "@mui/material",
+                // "react-bootstrap",              
+              ],
+            }
+          : { noExternal: [] }),
+      }),
+      cjsInterop({
+        // List of CJS dependencies that require interop
+        dependencies: [
+          "@mui/material/*",
+          // "react-bootstrap/*"
+        ],
+      }),
+      vercel(),
+      vercelSsr(),
+    ],
+  };
+});
